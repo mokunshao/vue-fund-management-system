5fd3fab11f619dc430c6d79aacca407fa7f15294
mokunshao <1627510957@qq.com>
2019-04-24 02:43:49 +0800

    备份

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
+const db = require("./config/keys").MongoURI;
 
-// mongoose.connect('')
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"), err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("hello world");
