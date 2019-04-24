42218e13b31b1e5e0436edcab71a9ddf62e65418
mokunshao <1627510957@qq.com>
2019-04-24 23:00:13 +0800

    备份

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 const db = require("./config/keys").MongoURI;
+const users = require("./routes/api/users");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -11,6 +16,8 @@ mongoose
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.use("/api/users/", users);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
