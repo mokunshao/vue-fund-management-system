d2e9e6a0ba9059dce56549955fdaa165355f977c
mokunshao <1627510957@qq.com>
2019-04-29 17:22:40 +0800

    备份

---
+++ client/src/components/Dialog.vue
@@ -0,0 +1,97 @@
+<template>
+  <el-dialog :title="dialog.title" :visible.sync="dialog.show">
+    <el-form ref="form" :model="form" :rules="form_rules" label-width="120px">
+      <el-form-item label="收支类型:">
+        <el-select v-model="form.type" placeholder="收支类型">
+          <el-option
+            v-for="(formtype, index) in format_type_list"
+            :key="index"
+            :label="formtype"
+            :value="formtype"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item prop="description" label="收支描述:">
+        <el-input v-model="form.description"></el-input>
+      </el-form-item>
+
+      <el-form-item prop="income" label="收入:">
+        <el-input v-model="form.income"></el-input>
+      </el-form-item>
+
+      <el-form-item prop="expense" label="支出:">
+        <el-input v-model="form.expense"></el-input>
+      </el-form-item>
+
+      <el-form-item prop="cash" label="账户现金:">
+        <el-input v-model="form.cash"></el-input>
+      </el-form-item>
+
+      <el-form-item label="备注:">
+        <el-input type="textarea" v-model="form.remark"></el-input>
+      </el-form-item>
+
+      <el-form-item class="text_right">
+        <el-button @click="dialog.show = false">取消</el-button>
+        <el-button type="primary" @click="onSubmit()">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "Dialog",
+  props: { dialog: Object },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.$axios.post(`/api/profiles/add`, this.form).then(res => {
+            this.$message({
+              message: "保存成功！",
+              type: "success"
+            });
+            this.dialog.show = false;
+            this.$emit("update");
+          });
+        }
+      });
+    }
+  },
+  data() {
+    return {
+      form: {
+        type: "",
+        description: "",
+        income: "",
+        expense: "",
+        cash: "",
+        remark: ""
+        // id: ""
+      },
+      format_type_list: [
+        "提现",
+        "提现手续费",
+        "充值",
+        "优惠券",
+        "充值礼券",
+        "转账"
+      ],
+      form_rules: {
+        description: [
+          { required: true, message: "收支描述不能为空！", trigger: "blur" }
+        ],
+        income: [
+          { required: true, message: "收入不能为空！", trigger: "blur" }
+        ],
+        expense: [
+          { required: true, message: "支出不能为空！", trigger: "blur" }
+        ],
+        cash: [{ required: true, message: "账户不能为空！", trigger: "blur" }]
+      }
+    };
+  }
+};
+</script>
--- client/src/views/FundList.vue
+++ client/src/views/FundList.vue
@@ -1,48 +1,69 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column type="index" label="序号" align="center"></el-table-column>
-    <el-table-column prop="date" label="创建时间">
-      <template slot-scope="scope">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ scope.row.date.split('T')[0] }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="type" label="收支类型" align="center"></el-table-column>
-    <el-table-column prop="description" label="收支描述" align="center"></el-table-column>
-    <el-table-column prop="income" label="收入" align="center">
-      <template slot-scope="scope">
-        <span style="color:#00d053">+ {{ scope.row.income }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="expense" label="支出" align="center">
-      <template slot-scope="scope">
-        <span style="color:#f56767">- {{ scope.row.expense }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="cash" label="账户现金" align="center">
-      <template slot-scope="scope">
-        <span style="color:#4db3ff">{{ scope.row.cash }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="remark" label="备注" align="center"></el-table-column>
-    <el-table-column prop="operation" align="center" label="操作" width="150">
-      <template slot-scope="scope">
-        <el-button type="warning" icon="edit" size="small" @click="onEditMoney(scope.row)">编辑</el-button>
-        <el-button
-          type="danger"
-          icon="delete"
-          size="small"
-          @click="onDeleteMoney(scope.row,scope.$index)"
-        >删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-form :inline="true" ref="search_data" :model="search_data">
+      <!-- <el-form-item label="投标时间筛选:">
+        <el-date-picker v-model="search_data.startTime" type="datetime" placeholder="选择开始时间"></el-date-picker>--
+        <el-date-picker v-model="search_data.endTime" type="datetime" placeholder="选择结束时间"></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="small" icon="search" @click="onScreeoutMoney()">筛选</el-button>
+      </el-form-item>-->
+      <el-form-item class="btnRight">
+        <el-button type="primary" @click="onAddMoney()" size="small" icon="view">添加</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="index" label="序号" align="center"></el-table-column>
+      <el-table-column prop="date" label="创建时间">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.date.split('T')[0] }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="type" label="收支类型" align="center"></el-table-column>
+      <el-table-column prop="description" label="收支描述" align="center"></el-table-column>
+      <el-table-column prop="income" label="收入" align="center">
+        <template slot-scope="scope">
+          <span style="color:#00d053">+ {{ scope.row.income }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="expense" label="支出" align="center">
+        <template slot-scope="scope">
+          <span style="color:#f56767">- {{ scope.row.expense }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="cash" label="账户现金" align="center">
+        <template slot-scope="scope">
+          <span style="color:#4db3ff">{{ scope.row.cash }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="remark" label="备注" align="center"></el-table-column>
+      <el-table-column prop="operation" align="center" label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button type="warning" icon="edit" size="small" @click="onEditMoney(scope.row)">编辑</el-button>
+          <el-button
+            type="danger"
+            icon="delete"
+            size="small"
+            @click="onDeleteMoney(scope.row,scope.$index)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <Dialog :dialog="dialog" @update='getProfile'/>
+  </div>
 </template>
 
 <script>
+import Dialog from "@/components/Dialog";
+
 export default {
+  components: { Dialog },
   name: "FundList",
   methods: {
+    onAddMoney() {
+      this.dialog.show = true;
+    },
     onEditMoney() {
       console.log(123);
     },
@@ -60,7 +81,12 @@ export default {
   },
   data() {
     return {
-      tableData: []
+      tableData: [],
+      search_data: {},
+      dialog: {
+        show: false,
+        title: "we"
+      }
     };
   },
   created() {
@@ -68,3 +94,9 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.btnRight {
+  float: right;
+}
+</style>
