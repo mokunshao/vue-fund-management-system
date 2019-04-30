11e5d3f54c9151974f35a2a0a2fcb1ad201caa8f
mokunshao <1627510957@qq.com>
2019-04-30 20:47:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const db = require("./config/keys").MongoURI;
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const passport = require("passport");
+const path = require("path");
 
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,6 +18,13 @@ app.use("/api/profile/", profile);
 app.get("/api/test", (req, res) => {
   res.json("You can see me now.");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true, useFindAndModify: false })
